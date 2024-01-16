e586188b090d5740017f0743cd87de7c61d90855
sethpobanz <sethpobanz@gmail.com>
2024-01-16 11:52:33 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using Mission2Assignment;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        RollDice rd = new RollDice();
+
+        string numrolls = "";
+        System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine("How many dice rolls would you like to simulate?");
+        numrolls = System.Console.ReadLine();
+
+        int[] arrayRolls = rd.RollDiceMethod(numrolls); // Retrieve the array
+
+        // Count occurrences of each sum
+        int[] sumCounts = new int[11]; // Indices 0 to 10 represent sums 2 to 12
+
+        foreach (var roll in arrayRolls)
+        {
+            sumCounts[roll - 2]++;
+        }
+
+        // Calculate total number of rolls
+        int totalRolls = arrayRolls.Length;
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS\r\nEach \"*\" represents 1% of the total number of rolls.\r\nTotal number of rolls = 1000.");
+        // Print histogram
+        for (int i = 0; i < sumCounts.Length; i++)
+        {
+            int sum = i + 2; // Convert index back to sum value
+            double percentage = (double)sumCounts[i] / totalRolls * 100;
+
+            Console.WriteLine($"{sum}: {new string('*', (int)Math.Round(percentage))}");
+        }
+    }
+}
---
+++ RollDice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2Assignment
+{
+    internal class RollDice
+    {
+        public int[] RollDiceMethod(string numRolls)
+        {
+            int intNumRolls = int.Parse(numRolls);
+
+            int[] arrayRolls = new int[intNumRolls];
+
+            // Creating a Random object for generating random numbers
+            Random random = new Random();
+
+            // Loop through each element in the array
+            for (int i = 0; i < arrayRolls.Length; i++)
+            {
+                // Generate two random numbers between 1 and 6
+                int firstRoll = random.Next(1, 7);
+                int secondRoll = random.Next(1, 7);
+
+                // Add the two numbers and assign the result to the array element
+                arrayRolls[i] = firstRoll + secondRoll;
+            }
+
+            return arrayRolls; // Return the array
+        }
+
+    }
+}
